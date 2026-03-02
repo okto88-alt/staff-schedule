16380b0e0d5b366d61be9805096fb8d89a908665
okto88-alt <cs00.okto88@gmail.com>
2026-03-02 23:33:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -206,13 +206,24 @@ function openScheduleModal(staffId) {
     const fullImg = document.getElementById("modalFullCharacter");
     const video = document.getElementById("modalCharacterVideo");
     
-    // Smooth loading
-    fullImg.classList.remove("loaded");
-    fullImg.src = fullCharacterMap[staff.name] || staff.avatar;
-    fullImg.onload = () => {
-        fullImg.classList.add("loaded");
-    };
+fullImg.classList.remove("loaded");
 
+if(characterVideoMap[staff.name]){
+    video.src = characterVideoMap[staff.name];
+    video.style.display = "block";
+    fullImg.style.display = "none";
+    video.load();
+    video.play();
+}else{
+    video.style.display = "none";
+    fullImg.style.display = "block";
+    fullImg.src = fullCharacterMap[staff.name] || staff.avatar;
+}
+
+fullImg.onload = () => {
+    fullImg.classList.add("loaded");
+};
+    
     const roleClass = getRoleClass(staff.roleCode);
     const roleText = getRoleText(staff.role, staff.roleCode);
     modalRole.className = `role-badge ${roleClass}`;
