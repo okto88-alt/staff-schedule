2341e099fbad7c79955a02f36d5919f4599a443d
okto88-alt <cs00.okto88@gmail.com>
2026-03-03 06:05:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const fullCharacterMap = {
 const characterVideoMap = {
     "HERMAN": "assets/characters/video/herman_idle.webm",
     "ACIN": "assets/characters/video/acin_idle.webm",
+    "HARI": "assets/characters/video/hari_idle.webm",
     "HEWIN": "assets/characters/video/hewin_idle.webm"
 };
 
